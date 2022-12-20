2718811d96bfb694476de465319b005134582ae6
iamyunusali <aliyunus291@gmail.com>
2022-12-20 14:51:23 +0300

    config routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,14 @@ Rails.application.routes.draw do
     resources :deals, except: [:show]
   end
   devise_for :users
+  devise_scope :user do
+  get '/users/sign_out' => 'devise/sessions#destroy'
+  end
+
 
   authenticated :user do
-    root "categories#index", as: :groups_path
+    root "categories#index", as: :authenticated_root
   end
+
   root "users#index"
 end
